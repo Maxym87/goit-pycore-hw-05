4aaab1a27a8fed941ef5d808d27de8f59b791aae
Maks Shkafar <m.shkafar@gmail.com>
2025-04-02 20:00:10 +0200

    create func sum_profit

--- task_2.py
+++ task_2.py
@@ -1,5 +1,4 @@
 
-import re
 from collections import Callable
 
 def generator_numbers(text: str):
@@ -13,4 +12,11 @@ def generator_numbers(text: str):
             continue
 
 def sum_profit(text: str, func: Callable) -> float:
+    total = sum(func(text))
+
+    return total
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+total_income = sum_profit(text, generator_numbers)
+print(f"Загальний дохід: {total_income}")
         
